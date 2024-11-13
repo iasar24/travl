8d2883cbe060ce7b000c1939be04db338b3a1811
Arth Rawat <118300136+iasar24@users.noreply.github.com>
2024-11-13 21:16:36 +0530

    Add files via upload

---
+++ src/db/conn.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://127.0.0.1:27017/employee0",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true
+}).then(() => {
+    console.log(`connection successful`);
+}).catch((e)=>{
+    console.log(`no connection`);
+});
+// require('./db');
